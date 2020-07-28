06673093c3a839a467d9c9acca0883890cdafdb2
Matheus Jose de Moura <matheusjosemoura@hotmail.com>
2020-07-28 14:06:31 -0300

    Logo changes

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
+import LogoMMW from '../../assets/img/onlyMMW.png';
 import { FooterBase } from './styles';
 
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      <a href="https://github.com/matheusjmoura/">
+        <img src={LogoMMW} alt="Logo MM Branco" width="100" height="50" />
       </a>
       <p>
-        Orgulhosamente criado durante a
-        {' '}
-        <a href="https://www.alura.com.br/">
-          Imersão React da Alura
-        </a>
+        Desenvolvido por{' '}
+        <a href="https://github.com/matheusjmoura/mouraflix">Matheus Moura</a>
       </p>
     </FooterBase>
   );
