783a02297c7010f4f1d3c474cfa47d4e31391c67
Matheus Jose de Moura <matheusjosemoura@hotmail.com>
2020-07-29 02:40:38 -0300

    Adding page default and slider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './reset.css';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CadastroVideo from './pages/Register/Video';
+import CadastroCategoria from './pages/Register/Category';
+
+const notFound404 = () => <div>Página 404 - Not Found</div>;
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={notFound404} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
